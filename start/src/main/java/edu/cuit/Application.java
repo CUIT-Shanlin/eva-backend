006c48ef00ec3eb5916754f51a019db203dd9fcc
LittleStrange <zym1934944134@gmail.com>
2024-08-07 17:21:08 +0800

    feat: 增添主函数

--- start/src/main/java/edu/cuit/Application.java
+++ start/src/main/java/edu/cuit/Application.java
@@ -1,7 +1,16 @@
 package edu.cuit;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 程序主入口
  */
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
 }
