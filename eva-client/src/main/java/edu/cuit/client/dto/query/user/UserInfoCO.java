3d5876c24137555ebe1230a666062c3dc5576eb7
LittleStrange <zym1934944134@gmail.com>
2024-08-19 17:16:59 +0800

    feat: 用户相关co

---
+++ eva-client/src/main/java/edu/cuit/client/dto/query/user/UserInfoCO.java
@@ -0,0 +1,38 @@
+package edu.cuit.client.dto.query.user;
+
+import com.alibaba.cola.dto.ClientObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * 用户信息（包含路由数据等）
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Accessors(chain = true)
+public class UserInfoCO extends ClientObject {
+
+    /**
+     * 用户基本信息
+     */
+    private UserDetailCO info;
+
+    /**
+     * 路由列表
+     */
+    private List<RouterDetailCO> routerList;
+
+    /**
+     * 角色列表
+     */
+    private List<RoleDetailCO> roleList;
+
+    /**
+     * 按钮列表
+     */
+    private List<String> buttonList;
+
+}
