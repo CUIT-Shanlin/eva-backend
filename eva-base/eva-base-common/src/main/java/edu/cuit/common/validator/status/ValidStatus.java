7abb39b342098ed653ef57231495f9134ccca1e0
LittleStrange <zym1934944134@gmail.com>
2024-08-20 15:08:53 +0800

    feat: 状态值校验器

---
+++ eva-base/eva-base-common/src/main/java/edu/cuit/common/validator/status/ValidStatus.java
@@ -0,0 +1,28 @@
+package edu.cuit.common.validator.status;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+/**
+ * 状态有效值校验器，目标值必须在value数组中存在才算有效
+ * null值被看做有效
+ */
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = ValidStatusValidator.class)
+public @interface ValidStatus {
+
+    /**
+     * 有效值
+     */
+    int[] value() default {0,1};
+
+    String message() default "状态值无效";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+}
---
+++ eva-base/eva-base-common/src/main/java/edu/cuit/common/validator/status/ValidStatusValidator.java
@@ -0,0 +1,26 @@
+package edu.cuit.common.validator.status;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+/**
+ * 状态有效值校验器
+ */
+public class ValidStatusValidator implements ConstraintValidator<ValidStatus,Integer> {
+
+    private int[] validValues;
+
+    @Override
+    public boolean isValid(Integer value, ConstraintValidatorContext context) {
+        if (value == null) return true;
+        for (int validValue : validValues) {
+            if (validValue == value) return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void initialize(ValidStatus constraintAnnotation) {
+        validValues = constraintAnnotation.value();
+    }
+}
