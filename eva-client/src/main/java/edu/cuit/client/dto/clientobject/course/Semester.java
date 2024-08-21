639a68e06e319ad0a9478727af13e4a8cd2a85a8
Conquer_Solitude <3388659470@qq.com>
2024-08-21 13:51:08 +0800

    feat: 学期数据模型

---
+++ eva-client/src/main/java/edu/cuit/client/dto/clientobject/course/Semester.java
@@ -0,0 +1,34 @@
+package edu.cuit.client.dto.clientobject.course;
+
+import com.alibaba.cola.dto.ClientObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+/**
+ * 课程时间模型
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Accessors(chain = true)
+public class Semester extends ClientObject {
+    /**
+     * 课程id
+     */
+    private Integer id;
+
+    /**
+     * 上下学期，0为上，1为下
+     */
+    private Integer period;
+
+    /**
+     * 开始年份，如2023
+     */
+    private String startYear;
+
+    /**
+     * 结束年份
+     */
+    private String endYear;
+}
