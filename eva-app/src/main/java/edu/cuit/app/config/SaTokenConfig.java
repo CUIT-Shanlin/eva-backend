a400c6f225357ee247cbb2c0e67a6d56ba688895
LittleStrange <zym1934944134@gmail.com>
2024-09-04 20:37:01 +0800

    feat: satoken鉴权拦截器

---
+++ eva-app/src/main/java/edu/cuit/app/config/SaTokenConfig.java
@@ -0,0 +1,19 @@
+package edu.cuit.app.config;
+
+import cn.dev33.satoken.interceptor.SaInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 安全认证相关配置
+ */
+@Configuration
+public class SaTokenConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new SaInterceptor()).addPathPatterns("/**");
+        //TODO 启动鉴权拦截器
+    }
+}
