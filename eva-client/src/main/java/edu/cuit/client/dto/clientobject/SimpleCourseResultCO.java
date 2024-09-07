db9d503cb5885899f48270c505d35c7b4da2884a
Conquer_Solitude <3388659470@qq.com>
2024-09-07 19:25:14 +0800

    feat: 新增极简响应模型-一门课程模型

---
+++ eva-client/src/main/java/edu/cuit/client/dto/clientobject/SimpleCourseResultCO.java
@@ -0,0 +1,31 @@
+package edu.cuit.client.dto.clientobject;
+
+import com.alibaba.cola.dto.ClientObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+
+/**
+ * 极简响应模型-一门课程
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Accessors(chain = true)
+public class SimpleCourseResultCO extends ClientObject {
+
+    /**
+     * id
+     */
+    private Integer id;
+
+    /**
+     * 名称
+     */
+    private String name;
+
+    /**
+     * 教学老师姓名
+     */
+    private String teacherName;
+}
