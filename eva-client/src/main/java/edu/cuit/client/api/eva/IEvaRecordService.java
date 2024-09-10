648dc7c9e70d3fff50791bed250abc900cb62d1a
LittleStrange <zym1934944134@gmail.com>
2024-09-10 17:15:32 +0800

    feat: 评教信息业务接口类

---
+++ eva-client/src/main/java/edu/cuit/client/api/eva/IEvaRecordService.java
@@ -0,0 +1,7 @@
+package edu.cuit.client.api.eva;
+
+/**
+ * 评教记录相关业务接口
+ */
+public interface IEvaRecordService {
+}
---
+++ eva-client/src/main/java/edu/cuit/client/api/eva/IEvaStatisticsService.java
@@ -0,0 +1,7 @@
+package edu.cuit.client.api.eva;
+
+/**
+ * 评教统计相关业务接口
+ */
+public interface IEvaStatisticsService {
+}
