4ce91e414ee70c4cc5da4e792d48076b0c02a94f
LittleStrange <zym1934944134@gmail.com>
2024-09-10 22:27:55 +0800

    feat: 日志相关接口

---
+++ eva-adapter/src/main/java/edu/cuit/adapter/controller/LogController.java
@@ -0,0 +1,48 @@
+package edu.cuit.adapter.controller;
+
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import edu.cuit.client.dto.clientobject.PaginationQueryResultCO;
+import edu.cuit.client.dto.clientobject.log.LogModuleCO;
+import edu.cuit.client.dto.clientobject.log.OperateLogCO;
+import edu.cuit.client.dto.query.PagingQuery;
+import edu.cuit.client.dto.query.condition.GenericConditionalQuery;
+import edu.cuit.zhuyimeng.framework.common.result.CommonResult;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 系统日志相关接口
+ */
+@RestController
+@RequiredArgsConstructor
+@Validated
+@RequestMapping("/logs")
+public class LogController {
+
+    /**
+     * 分页获取日志
+     * @param query 分页查询模型
+     */
+    @PostMapping
+    @SaCheckPermission("system.log.query")
+    public CommonResult<PaginationQueryResultCO<OperateLogCO>> page(@RequestBody @Valid PagingQuery<GenericConditionalQuery> query) {
+        return null;
+    }
+
+    /**
+     * 获取全部日志模块
+     */
+    @PostMapping("/modules")
+    @SaCheckPermission("system.log.query")
+    public CommonResult<List<LogModuleCO>> getModules() {
+        return null;
+    }
+
+}
