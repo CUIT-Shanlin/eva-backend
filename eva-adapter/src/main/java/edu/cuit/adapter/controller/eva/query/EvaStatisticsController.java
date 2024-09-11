0e15af22219e4fe2a7f04e914ab87b7239f5609b
LittleStrange <zym1934944134@gmail.com>
2024-09-11 14:41:20 +0800

    refactor: 优化eva接口规范

--- eva-adapter/src/main/java/edu/cuit/adapter/controller/eva/query/EvaStatisticsController.java
+++ eva-adapter/src/main/java/edu/cuit/adapter/controller/eva/query/EvaStatisticsController.java
@@ -4,11 +4,20 @@ import cn.dev33.satoken.annotation.SaCheckPermission;
 import edu.cuit.client.dto.clientobject.eva.ScoreRangeCourseCO;
 import edu.cuit.client.dto.clientobject.eva.*;
 import edu.cuit.zhuyimeng.framework.common.result.CommonResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+/**
+ * 评教统计相关
+ */
+@RestController
+@RequiredArgsConstructor
+@Validated
 public class EvaStatisticsController {
-    /**
-     * 评教记录相关统计
-     */
+
+    // 评教记录相关统计
+
     /**
      * 获取评教分数统计基础信息
      * @param score 指定分数
@@ -32,9 +41,8 @@ public class EvaStatisticsController {
         return null;
     }
 
-    /**
-     * 评教看板相关
-     */
+    // 评教看板相关
+
     /**
      * 获取指定某一天的详细评教统计数据
      * @param day 指定的这一天和今天相差多少天，eg：0 =》 今天，-1 =》 昨天
@@ -48,6 +56,7 @@ public class EvaStatisticsController {
             @RequestParam (value = "semId",required = false) Integer semId){
         return null;
     }
+
     /**
      * 获取各个分数段中 课程的数目情况
      * @param num 获取多少个分数段的数据，分数段截取后段，如果有某个分数段 课程数目为0，应当忽略掉，不参与计算
@@ -59,6 +68,7 @@ public class EvaStatisticsController {
             @PathVariable ("interval") Integer interval){
         return null;
     }
+
     /**
      * 获取上个月和本月的评教数目，以有两个整数的List<Integer>形式返回，data[0]：上个月评教数目；data[1]：本月评教数目
      * @param semId 学期id
@@ -68,6 +78,7 @@ public class EvaStatisticsController {
             @RequestParam(value = "semId",required = false) Integer semId){
         return null;
     }
+
     /**
      * 获取指定过去一段时间内的详细评教统计数据
      * @param num 获取从今天开始往过去看 num 天（含今天）中，每天的新增评教数目
@@ -76,7 +87,7 @@ public class EvaStatisticsController {
      * @param semId 学期id
      */
     @GetMapping("/evaluate/moreCounts/{num}/{target}/{evaTarget}")
-    public CommonResult<PastTimeEvaDetailCO> GetEvaData(
+    public CommonResult<PastTimeEvaDetailCO> getEvaData(
             @RequestParam(value = "semId",required = false) Integer semId,
             @PathVariable ("num") Integer num,
             @PathVariable ("target") Integer target,
