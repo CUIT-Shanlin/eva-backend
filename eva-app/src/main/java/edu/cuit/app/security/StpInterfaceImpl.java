bbf014ce106c9eaa44a5f5708beacec6eb9319d8
LittleStrange <zym1934944134@gmail.com>
2024-09-11 15:02:29 +0800

    feat: satoken 权限实现类

---
+++ eva-app/src/main/java/edu/cuit/app/security/StpInterfaceImpl.java
@@ -0,0 +1,33 @@
+package edu.cuit.app.security;
+
+import cn.dev33.satoken.stp.StpInterface;
+import edu.cuit.domain.gateway.user.LdapPersonGateway;
+import edu.cuit.domain.gateway.user.UserGateway;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * 权限加载
+ */
+@Component
+@RequiredArgsConstructor
+public class StpInterfaceImpl implements StpInterface {
+
+    private final UserGateway userGateway;
+    private final LdapPersonGateway ldapPersonGateway;
+
+
+    //TODO 实现权限接口
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        return List.of();
+    }
+
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        return List.of();
+    }
+}
