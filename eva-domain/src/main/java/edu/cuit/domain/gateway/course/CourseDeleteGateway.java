4bbbc380f554b7ff8990f8c987f6b666aae38752
Conquer_Solitude <3388659470@qq.com>
2024-09-19 20:53:05 +0800

    feat:课程删除接口基本完成

---
+++ eva-domain/src/main/java/edu/cuit/domain/gateway/course/CourseDeleteGateway.java
@@ -0,0 +1,34 @@
+package edu.cuit.domain.gateway.course;
+
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 课程删除相关数据门户接口
+ */
+@Component
+public interface CourseDeleteGateway {
+
+    /**
+     * 批量删除某节课
+     *  @param semId 学期id
+     *  @param id 对应课程编号
+     *  @param startWeek 从哪一周开始删除
+     *  @param endWeek 从哪一周结束删除
+     * */
+    Void deleteCourses(Integer semId,Integer id,Integer startWeek,Integer endWeek);
+
+    /**
+     * 连带删除一门课程
+     *  @param semId 学期id
+     *  @param id 对应课程编号
+     * */
+    Void deleteCourse(Integer semId,Integer id);
+
+    /**
+     * 删除一个课程类型/批量删除课程类型
+     *   @param ids 课程数组
+     * */
+    Void deleteCourseType(List<Integer> ids);
+}
