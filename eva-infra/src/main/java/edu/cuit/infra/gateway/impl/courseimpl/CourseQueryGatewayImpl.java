0316fb03d393335e1db2786d216ce68c0026b211
Conquer_Solitude <3388659470@qq.com>
2024-10-02 21:16:25 +0800

    feat:courseimpl实现类

--- eva-infra/src/main/java/edu/cuit/infra/gateway/impl/courseimpl/CourseQueryGatewayImpl.java
+++ eva-infra/src/main/java/edu/cuit/infra/gateway/impl/courseimpl/CourseQueryGatewayImpl.java
@@ -4,31 +4,40 @@ package edu.cuit.infra.gateway.impl.courseimpl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.fasterxml.jackson.core.JacksonException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.cuit.client.bo.EvaProp;
 import edu.cuit.client.dto.clientobject.course.CourseDetailCO;
+import edu.cuit.client.dto.clientobject.course.SingleCourseCO;
 import edu.cuit.client.dto.clientobject.eva.CourseScoreCO;
 import edu.cuit.client.dto.clientobject.eva.EvaTemplateCO;
 import edu.cuit.client.dto.data.course.CoursePeriod;
+import edu.cuit.client.dto.data.course.CourseTime;
 import edu.cuit.client.dto.data.course.CourseType;
 import edu.cuit.client.dto.query.CourseQuery;
 import edu.cuit.client.dto.query.PagingQuery;
 
 import edu.cuit.client.dto.query.condition.CourseConditionalQuery;
 import edu.cuit.client.dto.query.condition.GenericConditionalQuery;
+import edu.cuit.client.dto.query.condition.MobileCourseQuery;
 import edu.cuit.domain.entity.course.*;
+import edu.cuit.domain.entity.user.biz.MenuEntity;
+import edu.cuit.domain.entity.user.biz.RoleEntity;
+import edu.cuit.domain.entity.user.biz.UserEntity;
 import edu.cuit.domain.gateway.course.CourseQueryGateway;
 import edu.cuit.infra.convertor.course.CourseConvertor;
+import edu.cuit.infra.convertor.user.UserConverter;
 import edu.cuit.infra.dal.database.dataobject.course.*;
 import edu.cuit.infra.dal.database.dataobject.eva.CourOneEvaTemplateDO;
 import edu.cuit.infra.dal.database.dataobject.eva.EvaTaskDO;
 import edu.cuit.infra.dal.database.dataobject.eva.FormRecordDO;
-import edu.cuit.infra.dal.database.dataobject.user.SysUserDO;
+import edu.cuit.infra.dal.database.dataobject.user.*;
 import edu.cuit.infra.dal.database.mapper.course.*;
 import edu.cuit.infra.dal.database.mapper.eva.CourOneEvaTemplateMapper;
 import edu.cuit.infra.dal.database.mapper.eva.EvaTaskMapper;
 import edu.cuit.infra.dal.database.mapper.eva.FormRecordMapper;
-import edu.cuit.infra.dal.database.mapper.user.SysUserMapper;
+import edu.cuit.infra.dal.database.mapper.user.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -39,6 +48,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class CourseQueryGatewayImpl implements CourseQueryGateway {
     private final CourseConvertor courseConvertor;
+    private final UserConverter userConverter;
     private final CourInfMapper courInfMapper;
     private final CourseMapper courseMapper;
     private final CourseTypeCourseMapper courseTypeCourseMapper;
@@ -49,6 +59,10 @@ public class CourseQueryGatewayImpl implements CourseQueryGateway {
     private final CourOneEvaTemplateMapper CoureOneEvaTemplateMapper;
     private final EvaTaskMapper evaTaskMapper;
     private final FormRecordMapper formRecordMapper;
+    private final SysRoleMapper roleMapper;
+    private final SysUserRoleMapper userRoleMapper;
+    private final SysMenuMapper menuMapper;
+    private final SysRoleMenuMapper roleMenuMapper;
     @Override
     public List<CourseEntity> page(PagingQuery<CourseConditionalQuery> courseQuery, Integer semId) {
         //获取semesterEntity
@@ -142,13 +156,7 @@ public class CourseQueryGatewayImpl implements CourseQueryGateway {
         return Optional.of(courseDetailCO);
 
     }
-    /**
-     * 根据课程ID和学期ID查询评价分数
-     *
-     * @param id   课程ID
-     * @param semId 学期ID
-     * @return 返回一个List，包含指定课程和学期的所有评价分数记录
-     */
+
     @Override
     public List<CourseScoreCO> findEvaScore(Integer id, Integer semId) {
 
@@ -156,35 +164,40 @@ public class CourseQueryGatewayImpl implements CourseQueryGateway {
         QueryWrapper<EvaTaskDO> evaTaskWrapper = new QueryWrapper<>();
         evaTaskWrapper.eq("cour_inf_id", id);
         EvaTaskDO evaTaskDO = evaTaskMapper.selectOne(evaTaskWrapper);
-        //根据评教任务id来找到评教表单记录数据
-        List<FormRecordDO> evaRecordDOS = formRecordMapper.selectList(new QueryWrapper<FormRecordDO>().eq("task_id", evaTaskDO.getId()));
-        Map<String,List<FormRecordDO>> map=new HashMap<>();
-        //TODO 根据prop来分组
-        //问题是怎么从json中获取prop字段
-        for(FormRecordDO formRecordDO:evaRecordDOS){
-            String prop=formRecordDO.getFormPropsValues();
+        //根据评教任务id来找到评教表单记录数据中的form_props_values
+        List<String> taskProps = formRecordMapper.selectList(new QueryWrapper<FormRecordDO>().eq("task_id", evaTaskDO.getId()))
+                .stream().map(formRecordDO -> formRecordDO.getFormPropsValues()).toList();
+        //将json形式的字符串转化成EvaProp对象
+        List<EvaProp> evaPropList = taskProps.stream().map(taskProp -> {
             try {
-                CourseScoreCO courseScoreCO = new ObjectMapper().readValue(prop, CourseScoreCO.class);
-                map.put(courseScoreCO.getProp(),map.getOrDefault(courseScoreCO.getProp(),new ArrayList<>()));
-                map.get(courseScoreCO.getProp()).add(formRecordDO);
+                return new ObjectMapper().readValue(taskProp, EvaProp.class);
             } catch (JsonProcessingException e) {
-               throw new RuntimeException(e);
+                throw new RuntimeException(e);
             }
+        }).toList();
+        //根据EvaProp中的prop进行分组
+        Map<String, List<EvaProp>> evaPropMap = evaPropList.stream().collect(Collectors.groupingBy(EvaProp::getProp));
+        List<CourseScoreCO> courseScoreCOList=new ArrayList<>();
+        for (Map.Entry<String, List<EvaProp>> stringListEntry : evaPropMap.entrySet()) {
+           CourseScoreCO courseScoreCO=new CourseScoreCO();
+          Double sum=0.0;
+          Double max=0.0;
+          Double min=0.0;
+           courseScoreCO.setProp(stringListEntry.getKey());
+           for (EvaProp evaProp : stringListEntry.getValue()) {
+               sum+=evaProp.getScore();
+               if(evaProp.getScore()>max){
+                   max= Double.valueOf(evaProp.getScore());
+               }
+               if(evaProp.getScore()<min){
+                   min= Double.valueOf(evaProp.getScore());
+               }
+           }
+           courseScoreCO.setAverScore( (sum/stringListEntry.getValue().size()));
+           courseScoreCO.setMaxScore(max);
+           courseScoreCO.setMinScore(min);
+           courseScoreCOList.add(courseScoreCO);
         }
-
-
-        //将FormRecordDO转化成CourseScoreCO对象
-        List<CourseScoreCO> courseScoreCOList = evaRecordDOS.stream()
-                .map(formRecordDO -> {
-                    try {
-                        return new ObjectMapper().readValue(formRecordDO.getFormPropsValues(), CourseScoreCO.class);
-                    } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
-                    }
-                }).toList();
-
-
-
         return courseScoreCOList;
     }
 
@@ -241,17 +254,42 @@ public class CourseQueryGatewayImpl implements CourseQueryGateway {
     }
 
     @Override
-    public List<SingleCourseEntity> getPeriodInfo(Integer semId, CourseQuery courseQuery) {
-        return null;
+    public List<SingleCourseCO> getPeriodInfo(Integer semId, CourseQuery courseQuery) {
+        //先根据courseQuery中的信息来查询courInfoDO列表(课程详情表)
+        List<CourInfDO> courInfDOS = courInfMapper.selectList(new QueryWrapper<CourInfDO>().eq("week", courseQuery.getWeek()).eq("day",courseQuery.getDay()).eq("start_time",courseQuery.getNum()));
+        //根据courseInfDo中的课程id和semid来筛选课程实体
+        List<CourseDO> courseDOS = courseMapper.selectList(new QueryWrapper<CourseDO>().eq("semester_id", semId).in("id", courInfDOS.stream().map(CourInfDO::getCourseId).toList()));
+
+        List<SingleCourseCO> singleCourseCOList = new ArrayList<>();
+        //遍历courseDo组装SingleCourseCo
+        for (CourseDO courseDO : courseDOS) {
+           SingleCourseCO singleCourseCO = new SingleCourseCO();
+           singleCourseCO.setId(getCourseInfo(courseDO).getId());
+           singleCourseCO.setTime(new CourseTime(courseQuery.getWeek(),courseQuery.getDay(),courseQuery.getNum(),courseQuery.getNum()+1));
+            singleCourseCO.setTeacherName(userMapper.selectById(courseDO.getTeacherId()).getName());
+            singleCourseCO.setName(subjectMapper.selectById(courseDO.getSubjectId()).getName());
+            //根据课程id到评教任务表中统计数量
+            singleCourseCO.setEvaNum(Math.toIntExact(evaTaskMapper.selectCount(new QueryWrapper<EvaTaskDO>().eq("cour_inf_id", courseDO.getId()))));
+            singleCourseCOList.add(singleCourseCO);
+        }
+
+
+
+        return singleCourseCOList;
     }
 
     @Override
     public Optional<SingleCourseEntity> getSingleCourseDetail(Integer id, Integer semId) {
-        return Optional.empty();
+        //ID是课程详情id
+        CourInfDO courInfDO = courInfMapper.selectById(id);
+        //构建courseEntity
+        CourseEntity courseEntity = toCourseEntity(courInfMapper.selectById(id).getCourseId(), semId);
+
+        return Optional.of(courseConvertor.toSingleCourseEntity(courseEntity,courInfDO));
     }
 
     @Override
-    public List<SingleCourseEntity> getPeriodCourse(Integer semId, CourseQuery courseQuery) {
+    public List<SingleCourseEntity> getPeriodCourse(Integer semId, MobileCourseQuery courseQuery) {
         return null;
     }
 
@@ -328,6 +366,36 @@ public class CourseQueryGatewayImpl implements CourseQueryGateway {
         }
         return finalResult;
 
+    }
+    public CourInfDO getCourseInfo( CourseDO course){
+
+        return  courInfMapper.selectById(course.getId());
+    }
+    private CourseEntity toCourseEntity(Integer courseId,Integer semId){
+        //构造semester
+        SemesterEntity semesterEntity = getSemester(semId);
+        //构造courseDo
+        CourseDO courseDO = courseMapper.selectOne(new QueryWrapper<CourseDO>().eq("id", courseId).eq("semester_id", semId));
+        //构造subject
+        SubjectEntity subjectEntity = courseConvertor.toSubjectEntity(subjectMapper.selectById(courseDO.getSubjectId()));
+        //构造userEntity
+        UserEntity userEntity =toUserEntity(courseMapper.selectById(courseId).getTeacherId());
+        return courseConvertor.toCourseEntity(courseDO,subjectEntity,userEntity,semesterEntity);
+    }
+    private UserEntity toUserEntity(Integer userId){
+        //得到uer对象
+        SysUserDO userDO = userMapper.selectById(userId);
+        //根据userId找到角色id集合
+        List<Integer> roleIds = userRoleMapper.selectList(new QueryWrapper<SysUserRoleDO>().eq("user_id", userId)).stream().map(SysUserRoleDO::getRoleId).toList();
+        //根据角色id集合找到角色对象集合
+        List<RoleEntity> roleEntities = roleMapper.selectList(new QueryWrapper<SysRoleDO>().in("id", roleIds)).stream().map(roleDO -> userConverter.toRoleEntity(roleDO)).toList();
+        //根据角色id集合找到角色菜单表中的菜单id集合
+        List<Integer> menuIds = roleMenuMapper.selectList(new QueryWrapper<SysRoleMenuDO>().in("role_id", roleIds)).stream().map(SysRoleMenuDO::getMenuId).toList();
+        //根据menuids找到菜单对象集合
+        List<MenuEntity> menuEntities = menuMapper.selectList(new QueryWrapper<SysMenuDO>().in("id", menuIds)).stream().map(menuDO -> userConverter.toMenuEntity(menuDO)).toList();
+
+
+        return userConverter.toUserEntity(userDO,roleEntities,menuEntities);
     }
 
 }
