64570d2ffcbadd066a87d083c9779bb0af7ca714
LittleStrange <zym1934944134@gmail.com>
2024-10-02 13:38:01 +0800

    refactor: 改变子菜单修改逻辑

--- eva-infra/src/main/java/edu/cuit/infra/convertor/user/MenuConvertor.java
+++ eva-infra/src/main/java/edu/cuit/infra/convertor/user/MenuConvertor.java
@@ -3,16 +3,22 @@ package edu.cuit.infra.convertor.user;
 import edu.cuit.client.dto.clientobject.user.MenuCO;
 import edu.cuit.infra.convertor.EntityFactory;
 import edu.cuit.infra.dal.database.dataobject.user.SysMenuDO;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
+import org.mapstruct.TargetType;
 
 /**
  * 权限菜单对象转换器
  */
 @Mapper(componentModel = "spring",uses = EntityFactory.class)
-@DecoratedWith(MenuConverterDecorator.class)
 public interface MenuConvertor {
 
     MenuCO toMenuCO(SysMenuDO menuDO);
+
+    @AfterMapping
+    default void afterMenuDOtoMenuCO(SysMenuDO menuDO, @TargetType MenuCO menuCO) {
+        //TODO 处理子菜单
+    }
 
 }
