4bc205be5ee0274ca75c142f2849a9f47b78ce82
LittleStrange <1934944134@qq.com>
2024-10-03 20:38:15 +0800

    fix: 修复菜单gateway实现类实现方法

--- eva-infra/src/main/java/edu/cuit/infra/gateway/impl/user/MenuQueryGatewayImpl.java
+++ eva-infra/src/main/java/edu/cuit/infra/gateway/impl/user/MenuQueryGatewayImpl.java
@@ -2,6 +2,7 @@ package edu.cuit.infra.gateway.impl.user;
 
 import edu.cuit.client.dto.clientobject.user.MenuCO;
 import edu.cuit.client.dto.query.condition.MenuConditionalQuery;
+import edu.cuit.domain.entity.user.biz.MenuEntity;
 import edu.cuit.domain.gateway.user.MenuQueryGateway;
 
 import java.util.List;
@@ -9,12 +10,12 @@ import java.util.List;
 public class MenuQueryGatewayImpl implements MenuQueryGateway {
 
     @Override
-    public List<MenuCO> getMenus(MenuConditionalQuery query) {
+    public List<MenuEntity> getMenus(MenuConditionalQuery query) {
         return List.of();
     }
 
     @Override
-    public MenuCO getOne(Integer id) {
+    public MenuEntity getOne(Integer id) {
         return null;
     }
 }
