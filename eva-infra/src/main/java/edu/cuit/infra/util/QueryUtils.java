8cfea632381548c9ce5aeff947776a1424b2e7dc
LittleStrange <1934944134@qq.com>
2024-10-04 16:09:43 +0800

    feat: 更新查询工具类

--- eva-infra/src/main/java/edu/cuit/infra/util/QueryUtils.java
+++ eva-infra/src/main/java/edu/cuit/infra/util/QueryUtils.java
@@ -3,6 +3,8 @@ package edu.cuit.infra.util;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.support.SFunction;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import edu.cuit.client.dto.query.PagingQuery;
 import edu.cuit.client.dto.query.condition.GenericConditionalQuery;
 
 /**
@@ -15,32 +17,46 @@ public class QueryUtils {
                                                                            SFunction<T,?> createTimeProp,
                                                                            SFunction<T,?> updateTimeProp) {
         if (query.getStartCreateTime() != null) {
-            queryWrapper.or().gt(createTimeProp,query.getStartCreateTime());
+            queryWrapper.or().gt(createTimeProp,query.getStartCreateTime())
+                    .or().eq(createTimeProp,query.getStartCreateTime());
         }
         if (query.getEndCreateTime() != null) {
-            queryWrapper.or().lt(createTimeProp,query.getEndCreateTime());
+            queryWrapper.or().lt(createTimeProp,query.getEndCreateTime())
+                    .or().eq(createTimeProp,query.getEndCreateTime());
         }
         if (query.getStartUpdateTime() != null) {
-            queryWrapper.or().gt(updateTimeProp,query.getStartUpdateTime());
+            queryWrapper.or().gt(updateTimeProp,query.getStartUpdateTime())
+                    .or().eq(updateTimeProp,query.getStartUpdateTime());
         }
         if (query.getEndUpdateTime() != null) {
-            queryWrapper.or().lt(updateTimeProp,query.getEndUpdateTime());
+            queryWrapper.or().lt(updateTimeProp,query.getEndUpdateTime())
+                    .or().eq(updateTimeProp,query.getEndUpdateTime());
         }
     }
 
     public static <T,R extends GenericConditionalQuery> void fileTimeQuery(QueryWrapper<T> queryWrapper, R query) {
         if (query.getStartCreateTime() != null) {
-            queryWrapper.or().gt("createTime",query.getStartCreateTime());
+            queryWrapper.or().gt("createTime",query.getStartCreateTime())
+                    .or().eq("createTime",query.getStartCreateTime());
         }
         if (query.getEndCreateTime() != null) {
-            queryWrapper.or().lt("createTime",query.getEndCreateTime());
+            queryWrapper.or().lt("createTime",query.getEndCreateTime())
+                    .or().eq("createTime",query.getEndCreateTime());
         }
         if (query.getStartUpdateTime() != null) {
-            queryWrapper.or().gt("updateTime",query.getStartUpdateTime());
+            queryWrapper.or().gt("updateTime",query.getStartUpdateTime())
+                    .or().eq("updateTime",query.getStartUpdateTime());
+
         }
         if (query.getEndUpdateTime() != null) {
-            queryWrapper.or().lt("updateTime",query.getEndUpdateTime());
+            queryWrapper.or().lt("updateTime",query.getEndUpdateTime())
+                    .or().eq("updateTime",query.getEndUpdateTime());
+
         }
+    }
+
+    public static <T> Page<T> createPage(PagingQuery<?> pageQuery) {
+        return Page.of(pageQuery.getPage(),pageQuery.getSize());
     }
 
 }
