8cc15a91bf35b81b6ca62d331087d4c5e372bc89
LittleStrange <1934944134@qq.com>
2024-10-15 17:42:06 +0800

    feat: 更新发送消息接口和业务接口

--- eva-adapter/src/main/java/edu/cuit/adapter/controller/MessageController.java
+++ eva-adapter/src/main/java/edu/cuit/adapter/controller/MessageController.java
@@ -1,6 +1,8 @@
 package edu.cuit.adapter.controller;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
+import cn.dev33.satoken.annotation.SaCheckPermission;
+import edu.cuit.client.dto.data.msg.WebsocketRequestMsg;
 import edu.cuit.client.dto.data.msg.WebsocketResponseMsg;
 import edu.cuit.client.validator.status.ValidStatus;
 import edu.cuit.zhuyimeng.framework.common.result.CommonResult;
@@ -36,7 +38,7 @@ public class MessageController {
      * @param num 指定消息数目，（负数或者null：全部）
      * @param type 消息类型（0：待办，1：通知，2：提醒，3：警告；null或者负数：全部）
      */
-    @GetMapping("/msg/tips/myNum/{num}/{type}")
+    @GetMapping("/tips/myNum/{num}/{type}")
     @SaCheckLogin
     public CommonResult<List<WebsocketResponseMsg>> getUserTargetAmountAndTypeMsg(@PathVariable("num") Integer num,
                                                                                   @PathVariable("type") @ValidStatus(value = {0,1,2,3}, message = "消息类型只能为0,1,2,3,负数或空") Integer type) {
@@ -48,7 +50,7 @@ public class MessageController {
      * @param id 消息id
      * @param isDisplayed 待改成的显示状态，0：未显示过，1：已显示过
      */
-    @PutMapping("/msg/tip/isDisplayed")
+    @PutMapping("/tip/isDisplayed")
     @SaCheckLogin
     public CommonResult<Void> updateMsgDisplay(@RequestParam("id") Integer id,
                                                @RequestParam("isDisplayed") @ValidStatus(message = "显示状态只能为0或1") Integer isDisplayed) {
@@ -60,7 +62,7 @@ public class MessageController {
      * @param id 消息id
      * @param isRead 待改成的已读状态，0：未读，1：已读
      */
-    @PutMapping("/msg/tip/isRead")
+    @PutMapping("/tip/isRead")
     @SaCheckLogin
     public CommonResult<Void> updateMsgRead(@RequestParam("id") Integer id,
                                             @RequestParam("isRead") @ValidStatus(message = "已读状态只能为0或1") Integer isRead) {
@@ -71,9 +73,19 @@ public class MessageController {
      * 批量修改某种性质的消息的已读状态，（注：改为已读的同时，也要改为已显示）
      * @param mode 确定待批量修改的是普通消息还是评教消息，0: 普通消息；1：评教消息
      */
-    @PutMapping("/msg/tips/{mode}}")
+    @PutMapping("/tips/{mode}}")
     @SaCheckLogin
     public CommonResult<Void> updateMultipleMsgRead(@PathVariable("mode") @ValidStatus(message = "mode只能为0或1") Integer mode) {
+        return null;
+    }
+
+    /**
+     * 发送消息接口，主要用于管理员向用户发消息
+     * @param msg 消息对象
+     */
+    @PostMapping("/tips/send")
+    @SaCheckPermission("msg.tips.send")
+    public CommonResult<Void> sendMessage(@RequestBody WebsocketRequestMsg msg) {
         return null;
     }
 }
---
+++ eva-client/src/main/java/edu/cuit/client/api/IMsgService.java
@@ -0,0 +1,53 @@
+package edu.cuit.client.api;
+
+import edu.cuit.client.dto.data.msg.WebsocketRequestMsg;
+import edu.cuit.client.dto.data.msg.WebsocketResponseMsg;
+
+import java.util.List;
+
+/**
+ * 消息相关接口
+ */
+public interface IMsgService {
+
+    /**
+     * 获取当前用户自己的指定类型的所有消息
+     * @param type 消息类型（0：待办，1：通知，2：提醒，3：警告；null或者负数：全部）
+     * @param mode 确定是普通消息还是评教消息，0: 普通消息；1：评教消息，null或者负数：全部
+     */
+    List<WebsocketResponseMsg> getUserTargetTypeMsg(Integer type, Integer mode);
+
+    /**
+     * 获取用户自己指定数目的指定类型的消息
+     * @param num 指定消息数目，（负数或者null：全部）
+     * @param type 消息类型（0：待办，1：通知，2：提醒，3：警告；null或者负数：全部）
+     */
+    List<WebsocketResponseMsg> getUserTargetAmountAndTypeMsg(Integer num, Integer type);
+
+    /**
+     * 修改某条消息的显示状态
+     * @param id 消息id
+     * @param isDisplayed 待改成的显示状态，0：未显示过，1：已显示过
+     */
+    void updateMsgDisplay(Integer id, Integer isDisplayed);
+
+    /**
+     * 修改某条消息的已读状态
+     * @param id 消息id
+     * @param isRead 待改成的已读状态，0：未读，1：已读
+     */
+    void updateMsgRead(Integer id, Integer isRead);
+
+    /**
+     * 批量修改某种性质的消息的已读状态，（注：改为已读的同时，也要改为已显示）
+     * @param mode 确定待批量修改的是普通消息还是评教消息，0: 普通消息；1：评教消息
+     */
+    void updateMultipleMsgRead(Integer mode);
+
+    /**
+     * 发送消息
+     * @param msg 消息对象
+     */
+    void sendMessage(WebsocketRequestMsg msg);
+
+}
