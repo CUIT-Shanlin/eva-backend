44f9d5b102b48183f48d2e3dc5596ab3d1f92a8a
Conquer_Solitude <3388659470@qq.com>
2024-10-17 22:58:26 +0800

    feat:新增operate类型

---
+++ eva-app/src/main/java/edu/cuit/app/service/operate/UserCourseDetail.java
@@ -0,0 +1,59 @@
+package edu.cuit.app.service.operate;
+
+import edu.cuit.app.convertor.course.CourseConvertor;
+import edu.cuit.client.dto.clientobject.course.CourseDetailCO;
+import edu.cuit.client.dto.clientobject.course.CourseModelCO;
+import edu.cuit.client.dto.data.course.CoursePeriod;
+import edu.cuit.client.dto.data.course.CourseType;
+import edu.cuit.domain.entity.course.SingleCourseEntity;
+import edu.cuit.domain.gateway.course.CourseQueryGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class UserCourseDetail {
+    @Autowired
+    CourseQueryGateway courseQueryGateway;
+    @Autowired
+    CourseConvertor courseConvertor;
+    public  CourseDetailCO getUserCourseDetail(List<SingleCourseEntity> singleCourseEntities,Integer semId){
+        List<CourseType> typeList = courseQueryGateway.getCourseType(singleCourseEntities.get(0).getCourseEntity().getId());
+        CourseModelCO courseModelCO = courseConvertor.toCourseModelCO(singleCourseEntities.get(0).getCourseEntity(), courseQueryGateway.getLocation(singleCourseEntities.get(0).getCourseEntity().getId()));
+        //根据singleCourseEntities中的上课的星期数和startTime以及endTime，将课程分类
+        Map<String, List<SingleCourseEntity>> courseByDay = new HashMap<>();
+        for (SingleCourseEntity entity : singleCourseEntities) {
+           String dayOfWeek = entity.getDay().toString()+entity.getStartTime().toString()+entity.getEndTime().toString();
+            courseByDay.computeIfAbsent(dayOfWeek, k -> new ArrayList<>()).add(entity);
+        }
+        List<CoursePeriod> coursePeriodList = new ArrayList<>();
+        CoursePeriod temp=new CoursePeriod();
+        for (Map.Entry<String, List<SingleCourseEntity>> entry : courseByDay.entrySet()) {
+            temp.setStartTime(entry.getValue().get(0).getStartTime());
+            temp.setEndTime(entry.getValue().get(0).getEndTime());
+            temp.setDay(entry.getValue().get(0).getDay());
+            int startWeek=entry.getValue().get(0).getWeek();
+            int endWeek=entry.getValue().get(0).getWeek();
+            for (SingleCourseEntity singleCourseEntity : entry.getValue()) {
+                if(singleCourseEntity.getWeek()>=endWeek){
+                    endWeek=singleCourseEntity.getWeek();
+                }
+                if(singleCourseEntity.getWeek()<=startWeek){
+                    startWeek=singleCourseEntity.getWeek();
+                }
+            }
+            temp.setStartWeek(startWeek);
+            temp.setEndWeek(endWeek);
+            coursePeriodList.add(temp);
+            //清空temp
+            temp=new CoursePeriod();
+        }
+
+
+        return new CourseDetailCO().setCourseBaseMsg(courseModelCO).setDateList(coursePeriodList).setTypeList(typeList);
+    }
+}
