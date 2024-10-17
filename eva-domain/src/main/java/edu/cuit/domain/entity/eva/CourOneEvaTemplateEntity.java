f827e2de2cdf599d63699ece94704ed27594d479
zj <zhengjia12_4@qq.com>
2024-10-17 14:34:04 +0800

    feat:增加supplier

--- eva-domain/src/main/java/edu/cuit/domain/entity/eva/CourOneEvaTemplateEntity.java
+++ eva-domain/src/main/java/edu/cuit/domain/entity/eva/CourOneEvaTemplateEntity.java
@@ -3,8 +3,12 @@ package edu.cuit.domain.entity.eva;
 import com.alibaba.cola.domain.Entity;
 import edu.cuit.domain.entity.course.CourseEntity;
 import edu.cuit.domain.entity.course.SemesterEntity;
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+
+import java.util.function.Supplier;
 
 /**
  * 课程评教情况domain entity
@@ -22,12 +26,32 @@ public class CourOneEvaTemplateEntity {
     /**
      * 学期
      */
-    private SemesterEntity semester;
+    @Getter(AccessLevel.NONE)
+    private Supplier<SemesterEntity> semester;
+
+    private SemesterEntity sCache=null;
+
+    public synchronized SemesterEntity getSemester(){
+        if(sCache==null){
+            sCache=semester.get();
+        }
+        return sCache;
+    }
 
     /**
      * 该门课
      */
-    private CourseEntity course;
+    @Getter(AccessLevel.NONE)
+    private Supplier<CourseEntity> course;
+
+    private CourseEntity cCache=null;
+
+    public synchronized CourseEntity getCourse(){
+        if(cCache==null){
+            cCache=course.get();
+        }
+        return cCache;
+    }
 
     /**
      * 用到的评教模板的信息，用JSON表示的对象，eg: { name: ”某模板“, description: "模板描述", props: ["指标1", "指标2"] }
@@ -38,5 +62,6 @@ public class CourOneEvaTemplateEntity {
      * 该门课在这学期的统计数据，JSON表示的对象数组
      */
     private String courseStatistics;
+
 
 }
