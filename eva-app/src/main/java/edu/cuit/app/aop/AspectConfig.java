ea2b5b5e42d3fe3a1c73dae0a800d245c7f1c6c4
Conquer_Solitude <3388659470@qq.com>
2024-10-26 14:10:50 +0800

    fix：修改切面

--- eva-app/src/main/java/edu/cuit/app/aop/AspectConfig.java
+++ eva-app/src/main/java/edu/cuit/app/aop/AspectConfig.java
@@ -16,19 +16,25 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class AspectConfig {
     private final SemesterGateway semesterGateway;
-    @Before("@annotation(edu.cuit.app.aop.CheckSemId)")
-    public void checkSemId(JoinPoint joinPoint){
+    @Around("@annotation(edu.cuit.app.aop.CheckSemId)")
+    public Object checkSemId(ProceedingJoinPoint joinPoint) throws Throwable {
             Object[] args = joinPoint.getArgs();
             //找到参数名称为semId的参数
             MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
             String[] paramNames = methodSignature.getParameterNames();
             if (args != null && paramNames != null) {
                 for (int i = 0; i < args.length; i++) {
-                    if(paramNames[i].equals("semId")&& args[i] ==null){
-                        args[i]=semesterGateway.getNow().getId();
+                    if(paramNames[i].equals("semId")){
+                        if(args[i]==null||(Integer) args[i]<0) {
+                            Integer id = semesterGateway.getNow().getId();
+
+                            args[i]=id;
+                        }
+
                     }
                 }
             }
+        return joinPoint.proceed(args);
     }
 
 }
