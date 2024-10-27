34173753e57882dd54ca5cbbebcdc5b1c7880f48
Conquer_Solitude <3388659470@qq.com>
2024-10-27 16:10:58 +0800

    feat:新增批量修改课程类型模型

---
+++ eva-client/src/main/java/edu/cuit/client/dto/cmd/course/UpdateCoursesType.java
@@ -0,0 +1,27 @@
+package edu.cuit.client.dto.cmd.course;
+
+import com.alibaba.cola.dto.ClientObject;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.util.List;
+
+/**
+ * 批量修改课程对应类型的模型
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@Accessors(chain = true)
+public class UpdateCoursesType extends ClientObject {
+
+    /**
+     * 课程id数组
+     */
+    private List<Integer> courseIdList;
+
+    /**
+     * 待改成的课程类型id数组
+     */
+    private List<Integer> typeIdList;
+}
