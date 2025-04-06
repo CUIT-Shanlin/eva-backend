2fa3500b69907d4071137fa12172ccfbb2f6f363
LittleStrange <1934944134@qq.com>
2025-04-06 20:21:56 +0800

    perf: 优化冗余代码

--- eva-adapter/src/main/java/edu/cuit/adapter/controller/eva/update/UpdateEvaController.java
+++ eva-adapter/src/main/java/edu/cuit/adapter/controller/eva/update/UpdateEvaController.java
@@ -77,13 +77,11 @@ public class UpdateEvaController {
             StringBecomeCmd s = new StringBecomeCmd();
             Integer recordId = iEvaRecordService.putEvaTemplate(s.stringBecomeCmd(props));
             // 转换为 InputStream 并上传
-            log.info("图片开始转inputstream");
             InputStream[] inputStreams = new InputStream[images.length];
             try {
                 for (int i = 0; i < images.length; i++) {
                     inputStreams[i] = images[i].getInputStream();
                 }
-                log.info("已经转好了");
                 recordImageManager.uploadRecordImages(recordId, inputStreams);
             } finally {
                 // 确保关闭所有流
