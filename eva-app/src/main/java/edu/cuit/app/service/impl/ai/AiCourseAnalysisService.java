f20d130ed0e2bd22457f23ccb1832f3b56a21b2e
LittleStrange <1934944134@qq.com>
2025-04-12 16:27:19 +0800

    feat: ai建议逻辑

--- eva-app/src/main/java/edu/cuit/app/service/impl/ai/AiCourseAnalysisService.java
+++ eva-app/src/main/java/edu/cuit/app/service/impl/ai/AiCourseAnalysisService.java
@@ -1,26 +1,131 @@
 package edu.cuit.app.service.impl.ai;
 
+import com.alibaba.cola.exception.BizException;
+import dev.langchain4j.community.model.dashscope.QwenChatModel;
+import dev.langchain4j.memory.chat.MessageWindowChatMemory;
+import dev.langchain4j.service.AiServices;
 import edu.cuit.app.aop.CheckSemId;
 import edu.cuit.client.api.ai.IAiCourseAnalysisService;
 import edu.cuit.client.api.course.IUserCourseService;
 import edu.cuit.client.bo.ai.AiAnalysisBO;
+import edu.cuit.client.bo.ai.AiCourseSuggestionBO;
 import edu.cuit.client.dto.clientobject.course.CourseDetailCO;
+import edu.cuit.client.dto.clientobject.eva.EvaRecordCO;
+import edu.cuit.domain.entity.eva.EvaRecordEntity;
+import edu.cuit.domain.gateway.eva.EvaQueryGateway;
+import edu.cuit.domain.gateway.user.UserQueryGateway;
+import edu.cuit.infra.ai.aiservice.CourseAiServices;
+import edu.cuit.infra.ai.util.MessageUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class AiCourseAnalysisService implements IAiCourseAnalysisService {
 
     private final IUserCourseService userCourseService;
+    private final EvaQueryGateway evaQueryGateway;
+    private final UserQueryGateway userQueryGateway;
+
+    private final QwenChatModel qwenMaxChatModel;
+    private final QwenChatModel qwenTurboChatModel;
+    private final QwenChatModel deepseekChatModel;
 
     @Override
     @CheckSemId
     public AiAnalysisBO analysis(Integer semId, Integer teacherId) {
         List<CourseDetailCO> courseDetailsList = userCourseService.getUserCourseDetail(teacherId, semId);
 
-        return null;
+        Map<String, List<CourseDetailCO>> courseCollect = courseDetailsList.stream()
+                .collect(Collectors.groupingBy(cl -> cl.getCourseBaseMsg().getName()));
+
+        Map<String,List<EvaRecordEntity>> courseRecordMap = new HashMap<>();
+        courseCollect.forEach((name,courses) -> {
+            List<EvaRecordEntity> recordList = new ArrayList<>();
+            for (CourseDetailCO course : courses) {
+                recordList.addAll(evaQueryGateway.getRecordByCourse(course.getCourseBaseMsg().getId()));
+            }
+            courseRecordMap.put(courses.get(0).getCourseBaseMsg().getName(),recordList);
+        });
+
+        List<AiCourseSuggestionBO> courseSuggestionList = new ArrayList<>();
+        courseRecordMap.forEach((name,records) -> {
+            CourseAiServices aiService = AiServices.builder(CourseAiServices.class)
+                    .chatMemory(MessageWindowChatMemory.withMaxMessages(5))
+                    .chatLanguageModel(qwenMaxChatModel)
+                    .build();
+            List<String> reviews = records.parallelStream()
+                    .map(record -> {
+                        StringBuilder builder = new StringBuilder();
+                        builder.append("评价信息：").append(record.getTextValue()).append("评分信息：(");
+                        Map<String, Double> map = evaQueryGateway.getScorePropMapByProp(record.getFormPropsValues());
+                        map.forEach((prop,score) -> {
+                            builder.append(" ").append(prop).append(": ").append(score).append(" , ");
+                        });
+                        builder.append(" )");
+                        return builder.toString();
+                    }).toList();
+            AiCourseSuggestionBO suggestionBO = new AiCourseSuggestionBO();
+            String adventure = aiService.analyseAdventure(MessageUtils.convertToList(reviews), name);
+            String drawbacks = aiService.analyseDrawbacks();
+            String suggestion = aiService.suggestion();
+            suggestionBO
+                    .setCourseName(name)
+                    .setAdventures(adventure)
+                    .setDrawbacks(drawbacks)
+                    .setSuggestion(suggestion)
+                    .setBeEvaNumCount(records.size())
+                    .setHighScoreBeEvaCount((int) records.parallelStream()
+                            .filter(record -> evaQueryGateway.getScoreFromRecord(record.getFormPropsValues()).get() >= 95)
+                            .count());
+            courseSuggestionList.add(suggestionBO);
+        });
+
+        CourseAiServices aiService = AiServices.builder(CourseAiServices.class)
+                .chatLanguageModel(deepseekChatModel)
+                .build();
+
+        List<String> courseSuggestionStrings = new ArrayList<>();
+        for (AiCourseSuggestionBO suggestion : courseSuggestionList) {
+            String builder = "课程名称：" + suggestion.getCourseName() +
+                    " , 优点：" + suggestion.getAdventures() +
+                    " , 缺点：" + suggestion.getDrawbacks() +
+                    " , 改进方案：" + suggestion.getSuggestion();
+            courseSuggestionStrings.add(builder);
+        }
+
+        int totalBeEvaCount = courseRecordMap.values().stream()
+                .mapToInt(List::size)
+                .sum();
+
+        int highScoreBeEvaCount = courseRecordMap.values().stream()
+                .flatMap(List::stream)
+                .filter(record -> evaQueryGateway.getScoreFromRecord(record.getFormPropsValues()).get() >= 95)
+                .toList()
+                .size();
+
+        String overallSuggestion = aiService.overallSuggestion(MessageUtils.convertToList(courseSuggestionStrings));
+
+        AiAnalysisBO aiAnalysis = new AiAnalysisBO();
+        aiAnalysis
+                .setCourseSuggestions(courseSuggestionList)
+                .setOverallReport(overallSuggestion)
+                .setTeacherName(userQueryGateway.findById(teacherId).orElseThrow(() -> {
+                    BizException bizException = new BizException("导出报告失败，请联系管理员");
+                    log.error("根据用户id获取用户失败",bizException);
+                    return bizException;
+                }).getName())
+                .setTotalBeEvaCount(totalBeEvaCount)
+                .setHighScoreEvaCount(highScoreBeEvaCount);
+
+        return aiAnalysis;
     }
 }
---
+++ eva-infra/src/main/java/edu/cuit/infra/ai/aiservice/PromptConstants.java
@@ -0,0 +1,10 @@
+package edu.cuit.infra.ai.aiservice;
+
+public interface PromptConstants {
+
+    String SYSTEM_PROMPT = """
+            你是智慧教育小助手，用于帮助改进大学教师教学质量，本系统为一个评教系统，用于智慧安排老师之间进行听课打分，并收集其他老师对该老师的打分情况和评语。
+            回答问题不要对你的回答进行总结。
+            """;
+
+}
