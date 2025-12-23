4a31734471cce411d186b6f48a27122891c1383d
Lystran <1934944134@qq.com>
2025-12-23 23:09:02 +0800

    refactor: 清理评教查询门面接口冗余 import

--- bc-evaluation-infra/src/main/java/edu/cuit/infra/bcevaluation/query/EvaQueryRepo.java
+++ bc-evaluation-infra/src/main/java/edu/cuit/infra/bcevaluation/query/EvaQueryRepo.java
@@ -1,13 +1,5 @@
 package edu.cuit.infra.bcevaluation.query;
 
-import edu.cuit.client.dto.query.PagingQuery;
-import edu.cuit.client.dto.query.condition.GenericConditionalQuery;
-import edu.cuit.domain.entity.PaginationResultEntity;
-import edu.cuit.domain.entity.eva.EvaTemplateEntity;
-
-import java.util.List;
-import java.util.Optional;
-
 /**
  * 评教读侧 QueryRepo（将复杂查询与组装从 gateway 中抽离）。
  *
