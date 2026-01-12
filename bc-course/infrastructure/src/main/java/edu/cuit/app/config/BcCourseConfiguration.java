abdfe1227ae5c49a1786f5dd61d86496a44cd525
Lystran <1934944134@qq.com>
2026-01-12 18:04:19 +0800

    refactor: 装配 ClassroomQueryUseCase（保持行为不变）

--- bc-course/infrastructure/src/main/java/edu/cuit/app/config/BcCourseConfiguration.java
+++ bc-course/infrastructure/src/main/java/edu/cuit/app/config/BcCourseConfiguration.java
@@ -57,6 +57,7 @@ import edu.cuit.bc.course.application.usecase.DeleteCoursesEntryUseCase;
 import edu.cuit.bc.course.application.usecase.ImportCourseFileUseCase;
 import edu.cuit.bc.course.application.usecase.ImportCourseFileGatewayEntryUseCase;
 import edu.cuit.bc.course.application.usecase.IsCourseImportedUseCase;
+import edu.cuit.bc.course.application.usecase.ClassroomQueryUseCase;
 import edu.cuit.bc.course.application.usecase.SemesterQueryUseCase;
 import edu.cuit.bc.course.application.usecase.UpdateCourseInfoUseCase;
 import edu.cuit.bc.course.application.usecase.UpdateCourseTypeEntryUseCase;
@@ -79,6 +80,7 @@ import edu.cuit.bc.course.application.usecase.UpdateCourseGatewayEntryUseCase;
 import edu.cuit.bc.course.application.usecase.UpdateCoursesEntryUseCase;
 import edu.cuit.bc.course.application.usecase.UpdateSelfCourseEntryUseCase;
 import edu.cuit.bc.template.application.CourseTemplateLockService;
+import edu.cuit.domain.gateway.ClassroomGateway;
 import edu.cuit.domain.gateway.SemesterGateway;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -113,6 +115,11 @@ public class BcCourseConfiguration {
     @Bean
     public SemesterQueryUseCase semesterQueryUseCase(SemesterGateway semesterGateway) {
         return new SemesterQueryUseCase(semesterGateway);
+    }
+
+    @Bean
+    public ClassroomQueryUseCase classroomQueryUseCase(ClassroomGateway classroomGateway) {
+        return new ClassroomQueryUseCase(classroomGateway);
     }
 
     @Bean
