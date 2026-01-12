1aebda2402facbd765c50ae0c72c32da889014f4
Lystran <1934944134@qq.com>
2026-01-12 18:47:23 +0800

    refactor: ICourseTypeServiceImpl 委托 CourseTypeUseCase

--- eva-app/src/main/java/edu/cuit/app/service/impl/course/ICourseTypeServiceImpl.java
+++ eva-app/src/main/java/edu/cuit/app/service/impl/course/ICourseTypeServiceImpl.java
@@ -1,8 +1,6 @@
 package edu.cuit.app.service.impl.course;
 
-import edu.cuit.app.convertor.PaginationBizConvertor;
-
-import edu.cuit.app.convertor.course.CourseBizConvertor;
+import edu.cuit.bc.course.application.usecase.CourseTypeUseCase;
 import edu.cuit.client.api.course.ICourseTypeService;
 import edu.cuit.client.dto.clientobject.PaginationQueryResultCO;
 import edu.cuit.client.dto.cmd.course.UpdateCourseTypeCmd;
@@ -10,68 +8,50 @@ import edu.cuit.client.dto.cmd.course.UpdateCoursesToTypeCmd;
 import edu.cuit.client.dto.data.course.CourseType;
 import edu.cuit.client.dto.query.PagingQuery;
 import edu.cuit.client.dto.query.condition.GenericConditionalQuery;
-import edu.cuit.domain.entity.PaginationResultEntity;
-import edu.cuit.domain.entity.course.CourseTypeEntity;
-import edu.cuit.domain.gateway.course.CourseDeleteGateway;
-import edu.cuit.domain.gateway.course.CourseQueryGateway;
-import edu.cuit.domain.gateway.course.CourseUpdateGateway;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ICourseTypeServiceImpl implements ICourseTypeService {
-    private final CourseQueryGateway courseQueryGateway;
-    private final CourseUpdateGateway courseUpdateGateway;
-    private final CourseDeleteGateway courseDeleteGateway;
-    private final CourseBizConvertor courseConvertor;
-    private final PaginationBizConvertor pageConvertor;
+    private final CourseTypeUseCase courseTypeUseCase;
+
     @Override
     public PaginationQueryResultCO<CourseType> pageCourseType(PagingQuery<GenericConditionalQuery> courseQuery) {
-        PaginationResultEntity<CourseTypeEntity> entity = courseQueryGateway.pageCourseType(courseQuery);
-        List<CourseType> list = entity.getRecords().stream().map(courseConvertor::toCourseType).toList();
-        return pageConvertor.toPaginationEntity(entity,list);
+        return courseTypeUseCase.pageCourseType(courseQuery);
     }
 
     @Override
     public List<CourseType> allCourseType() {
-        PaginationResultEntity<CourseTypeEntity> entity = courseQueryGateway.pageCourseType(null);
-        return entity.getRecords().stream().map(courseConvertor::toCourseType).toList();
+        return courseTypeUseCase.allCourseType();
     }
 
     @Override
     public void updateCourseType(UpdateCourseTypeCmd courseType) {
-        courseUpdateGateway.updateCourseType(courseType);
+        courseTypeUseCase.updateCourseType(courseType);
     }
 
     @Override
     public void addCourseType(CourseType courseType) {
-        courseUpdateGateway.addCourseType(courseType);
-
+        courseTypeUseCase.addCourseType(courseType);
     }
 
     @Override
     public void deleteCourseType(Integer id) {
-        List<Integer> ids=null;
-        if(id!=null){
-            ids=new ArrayList<>();
-            ids.add(id);
-        }
-        courseDeleteGateway.deleteCourseType(ids);
+        courseTypeUseCase.deleteCourseType(id);
     }
 
     @Override
     public void deleteCoursesType(List<Integer> ids) {
-        courseDeleteGateway.deleteCourseType(ids);
-
+        courseTypeUseCase.deleteCoursesType(ids);
     }
 
     @Override
     public Void updateCoursesType(UpdateCoursesToTypeCmd updateCoursesToTypeCmd) {
-       courseUpdateGateway.updateCoursesType(updateCoursesToTypeCmd);
+        courseTypeUseCase.updateCoursesType(updateCoursesToTypeCmd);
         return null;
     }
 }
