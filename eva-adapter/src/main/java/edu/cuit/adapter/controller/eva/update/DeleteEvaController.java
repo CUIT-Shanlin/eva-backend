7809cc2d5c61ef06fadbc18ccff694de4b78c97a
Lystran <1934944134@qq.com>
2026-01-13 10:57:47 +0800

    refactor: 收敛 DeleteEvaController 依赖与返回表达（行为不变）

--- eva-adapter/src/main/java/edu/cuit/adapter/controller/eva/update/DeleteEvaController.java
+++ eva-adapter/src/main/java/edu/cuit/adapter/controller/eva/update/DeleteEvaController.java
@@ -2,13 +2,11 @@ package edu.cuit.adapter.controller.eva.update;
 
 import cn.dev33.satoken.annotation.SaCheckPermission;
 import edu.cuit.client.api.eva.IEvaRecordService;
-import edu.cuit.client.api.eva.IEvaTaskService;
 import edu.cuit.client.api.eva.IEvaTemplateService;
 import edu.cuit.common.enums.LogModule;
 import edu.cuit.zhuyimeng.framework.common.result.CommonResult;
 import edu.cuit.zhuyimeng.framework.logging.aspect.annotation.OperateLog;
 import edu.cuit.zhuyimeng.framework.logging.aspect.enums.OperateLogType;
-import edu.cuit.zhuyimeng.framework.logging.utils.LogUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -24,7 +22,6 @@ import java.util.List;
 public class DeleteEvaController {
     private final IEvaRecordService iEvaRecordService;
     private final IEvaTemplateService iEvaTemplateService;
-    private final IEvaTaskService iEvaTaskService;
     //删除
     /**
      *删除一条评教记录，删除之后，相当于用户没有进行过这次评教
@@ -36,7 +33,7 @@ public class DeleteEvaController {
     public CommonResult<Void> deleteOneEvaLogById(
             @RequestParam(value = "id") Integer id){
         iEvaRecordService.deleteOneEvaLogById(id);
-        return CommonResult.success(null);
+        return success();
     }
     /**
      * 批量删除评教记录，删除之后，相当于用户没有进行过这次评教，id集合放请求体
@@ -48,7 +45,7 @@ public class DeleteEvaController {
     public CommonResult<Void> deleteEvaLogsById(
             @RequestBody List<Integer> ids){
         iEvaRecordService.deleteEvaLogsById(ids);
-        return CommonResult.success(null);
+        return success();
     }
     /**
      * 删除评教模板
@@ -60,7 +57,7 @@ public class DeleteEvaController {
     public CommonResult<Void> deleteEvaTemplateById(
             @RequestParam(value = "templateId") Integer templateId){
         iEvaTemplateService.deleteEvaTemplateById(templateId);
-        return CommonResult.success(null);
+        return success();
     }
     /**
      * 批量删除模板，后端要再次检验是否可被删除
@@ -72,7 +69,10 @@ public class DeleteEvaController {
     public CommonResult<Void> deleteEvaTemplatesById(
             @RequestBody List<Integer> ids ){
         iEvaTemplateService.deleteEvaTemplatesById(ids);
-        return CommonResult.success(null);
+        return success();
     }
 
+    private static CommonResult<Void> success() {
+        return CommonResult.success(null);
+    }
 }
