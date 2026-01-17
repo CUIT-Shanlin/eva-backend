960dfbaf0c1f40b2efe23792c5e33960c830cd36
Lystran <1934944134@qq.com>
2026-01-17 14:02:53 +0800

    refactor: 归位 FileImportExec 到 bc-course-infra

--- bc-course/infrastructure/src/main/java/edu/cuit/app/service/operate/course/update/FileImportExec.java
+++ bc-course/infrastructure/src/main/java/edu/cuit/app/service/operate/course/update/FileImportExec.java
@@ -1,19 +1,11 @@
 package edu.cuit.app.service.operate.course.update;
 
 import edu.cuit.client.bo.CourseExcelBO;
-import edu.cuit.client.dto.clientobject.SemesterCO;
-import edu.cuit.client.dto.clientobject.course.SubjectCO;
-import edu.cuit.domain.entity.course.SingleCourseEntity;
-import edu.cuit.infra.dal.database.dataobject.course.CourseDO;
-import edu.cuit.infra.dal.database.dataobject.course.SubjectDO;
-import org.springframework.stereotype.Component;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
 
 public class FileImportExec {
     //科目集合
