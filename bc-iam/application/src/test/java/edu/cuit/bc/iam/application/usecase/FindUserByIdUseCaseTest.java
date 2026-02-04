d8804521eb81a15bad546f2999fe08b45ac5ed4b
Lystran <me@lystran.com>
2026-02-04 14:52:10 +0800

    refactor(iam): FindUserByIdUseCaseTest 去 UserEntity 编译期依赖

--- bc-iam/application/src/test/java/edu/cuit/bc/iam/application/usecase/FindUserByIdUseCaseTest.java
+++ bc-iam/application/src/test/java/edu/cuit/bc/iam/application/usecase/FindUserByIdUseCaseTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.cuit.bc.iam.application.port.UserEntityQueryPort;
 import edu.cuit.domain.entity.PaginationResultEntity;
-import edu.cuit.domain.entity.user.biz.UserEntity;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 
@@ -26,21 +25,20 @@ class FindUserByIdUseCaseTest {
         private Integer id;
 
         @Override
-        public Optional<UserEntity> findById(Integer id) {
+        public Optional<?> findById(Integer id) {
             this.findByIdCalls++;
             this.id = id;
             return Optional.empty();
         }
 
         @Override
-        public Optional<UserEntity> findByUsername(String username) {
+        public Optional<?> findByUsername(String username) {
             throw new UnsupportedOperationException();
         }
 
         @Override
-        public PaginationResultEntity<UserEntity> page(edu.cuit.client.dto.query.PagingQuery<edu.cuit.client.dto.query.condition.GenericConditionalQuery> query) {
+        public PaginationResultEntity<?> page(edu.cuit.client.dto.query.PagingQuery<edu.cuit.client.dto.query.condition.GenericConditionalQuery> query) {
             throw new UnsupportedOperationException();
         }
     }
 }
-
